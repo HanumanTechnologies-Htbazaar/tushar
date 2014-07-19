93c6a113a825a61e0da2b53b26d36b7f5bd17ef4
TUSHAR <TUSHAR@TUSHAR-PC>
2014-07-19 23:52:03 +0530

    First commit...

---
+++ againGIT/src/againGIT/Main.java
@@ -0,0 +1,12 @@
+package againGIT;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello JAVA........");
+
+System.out.println("You are the Best....");
+	}
+
+}
